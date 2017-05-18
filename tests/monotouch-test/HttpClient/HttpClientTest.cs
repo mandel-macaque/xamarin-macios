c8c7a659cd075535ac946e3583cf863b3df54869
Manuel de la Pena <mandel@microsoft.com>
2017-05-18 13:05:42 +0200

    Add tests to ensure that setRequest is correctly set.

---
+++ tests/monotouch-test/HttpClient/HttpClientTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+#if XAMCORE_2_0
+using Foundation;
+#else
+using MonoTouch.Foundation;
+#endif
+using NUnit.Framework;
+
+
+namespace MonoTouchFixtures.HttpClient
+{
+	[TestFixture]
+	public class HttpClientTest
+	{
+		const int WaitTimeout = 5000;
+
+		[Test]
+		public void CFNetworkEnsureModifiabilityPostSend ()
+		{
+			var handler = new CFNetworkHandler ();
+			using (var client = new HttpClient (handler))
+			using (var request = new HttpRequestMessage (HttpMethod.Get, "http://xamarin.com")) {
+				Assert.DoesNotThrow (() => handler.AllowAutoRedirect = !handler.AllowAutoRedirect);
+				var t = Task.Factory.StartNew (() => {
+					client.SendAsync (request).Wait (WaitTimeout);
+					Assert.Throws<InvalidOperationException> (() => handler.AllowAutoRedirect = !handler.AllowAutoRedirect);
+				});
+			}
+		}
+
+		[Test]
+		public void NSUrlSessionEnsureModifiabilityPostSend ()
+		{
+			var handler = new NSUrlSessionHandler ();
+			using (var client = new HttpClient (handler))
+			using (var request = new HttpRequestMessage (HttpMethod.Get, "http://xamarin.com")) {
+				Assert.DoesNotThrow (() => handler.AllowAutoRedirect = !handler.AllowAutoRedirect);
+				var t = Task.Factory.StartNew (() => {
+					client.SendAsync (request).Wait (WaitTimeout);
+					Assert.Throws<InvalidOperationException> (() => handler.AllowAutoRedirect = !handler.AllowAutoRedirect);
+				});
+			}
+		}
+	}
+}
